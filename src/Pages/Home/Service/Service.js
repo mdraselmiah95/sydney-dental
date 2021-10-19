dfe971cf8438a34977a78ccc432d33f557871c4e
rasel <russelroy15@gmail.com>
2021-10-19 20:08:49 +0600

    final touch

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -7,20 +7,18 @@ const Service = (props) => {
   const { id, title, work, imgOne } = props.service;
   return (
     <div className="col-lg-4 col-md-6 text-center p-3 mb-4">
-      <div className="col">
-        <div className="card main-card d-flex align-items-center border-0">
-          <img src={imgOne} className="card-img-top w-25" alt="" />
-          <div className="card-body">
-            <h3 className="card-title service-h3">{title}</h3>
-            <p className="service-p">{work}</p>
-          </div>
-          <Link to={`/serviceDetails/${id}`}>
-            <button className="btn btn-outline-light">
-              <FontAwesomeIcon icon={faAngleDoubleRight} className="me-2" />
-              LEARN MORE
-            </button>
-          </Link>
+      <div className="card main-card d-flex align-items-center border-0">
+        <img src={imgOne} className="card-img-top w-25" alt="" />
+        <div className="card-body">
+          <h3 className="card-title service-h3">{title}</h3>
+          <p className="service-p">{work}</p>
         </div>
+        <Link to={`/serviceDetails/${id}`}>
+          <button className="btn btn-outline-light">
+            <FontAwesomeIcon icon={faAngleDoubleRight} className="me-2" />
+            LEARN MORE
+          </button>
+        </Link>
       </div>
     </div>
   );
